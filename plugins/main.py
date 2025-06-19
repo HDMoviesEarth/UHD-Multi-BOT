286ac75da0be0623505ea51c4ce1a1103642fbd0
HDMoviesEarth <diesengaming517@gmail.com>
2025-06-19 09:20:21 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -22,9 +22,6 @@ async def start_message(bot, message):
            InlineKeyboardButton("🚀 ꜱᴜᴩᴩᴏʀᴛ", url="https://t.me/UHDBots_Support")               
                ],[            
            InlineKeyboardButton("🗿 ᴜʜᴅ ɴᴇᴛᴡᴏʀᴋ™", url="https://t.me/UHD_NETWORK"),
-           InlineKeyboardButton("🤗 ʜᴇʟᴩ", callback_data="help"),
-           InlineKeyboardButton("🤖 ᴀʙᴏᴜᴛ", callback_data="about") 
-               ],[
            InlineKeyboardButton("📢 ᴜᴩᴅᴀᴛᴇꜱ", url="https://t.me/UHD_Bots")
               ]])
 
